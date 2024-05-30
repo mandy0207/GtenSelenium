842e414b9607d3a8beac9243a2a8b8df28e3e30e
shubham <shubhamcanvas63@gmail.com>
2024-05-30 16:20:17 +0530

    created automation scripts for practice

---
+++ src/selenium_practice/basic_f.java
@@ -0,0 +1,46 @@
+package selenium_practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class basic_f {
+	
+	public static void  main (String[] args ) throws InterruptedException {
+		/**
+		 * Ctrl+shift+o -> bring appropriate imports
+		 */
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		//opens a url
+		driver.get("https:demoblaze.com/");
+		
+		// grab a title
+		String title = driver.getTitle();
+		
+		System.out.println(title);
+		
+		driver.navigate().back();
+		
+		Thread.sleep(2000);
+		
+		driver.navigate().forward();
+		
+		 System.out.println( driver.getCurrentUrl());
+		 
+		 Thread.sleep(2000);
+		
+		 //will always close the current window/ tab
+		 driver.close();
+		 
+		//will close entire browser
+		driver.quit();
+	
+			
+	}
+}
+
+	
+	
+	
+
