4db52e0f7f6e1a33c4243a75c5b30e92bb88f4d7
shivT1 <shivam.shivhare.1517@gmail.com>
2024-05-30 16:58:11 +0530

    fixed

--- src/shivamcode/Homework.java
+++ src/shivamcode/Homework.java
@@ -21,6 +21,8 @@ public class Homework {
 			driver.findElement(By.xpath("//input[@class='password']")).sendKeys("Qwer@12345");
 			driver.findElement(By.xpath("//*[@class='buttons']//input[@value='Log in']")).click();
 			driver.findElement(By.xpath("(//ul[@class='list'])[1]/li[4]/a")).click();
+			
+			Thread.sleep(5000);
 
 	            // Print all products in the clothing section
 	            List<WebElement> allproducts = driver.findElements(By.className("product-title"));
@@ -28,17 +30,18 @@ public class Homework {
 	                String productName = product.getText();
 	                System.out.println(productName);
                     
-	                Thread.sleep(1000);
+	                Thread.sleep(5000);
 	                
 	                // click on the product is a Blue Jeans.
 	                if (productName.contains("Blue Jeans")) {
-	                    WebElement clicktheproduct = product.findElement(By.xpath("//*[contains(text(),'Blue Jean')]"));
+	                    WebElement clicktheproduct = driver.findElement(By.xpath("//*[contains(text(),'Blue Jean')]"));
 	                    clicktheproduct.click();
 	                    System.out.println("click the product: " + productName);
+	                    break;
 	                }
 	            }
 	        
-	        
+	          driver.quit();  
 	    }
 
 }
